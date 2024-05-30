0cb2091fb8c69eab38ad7a964ea64a92afb198c2
khadijahpassa <khadijahpassa@gmail.com>
2024-05-30 13:12:15 +0700

    landing odit

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,13 +6,16 @@
 <section id="hero">
     <div class="container h-100">
         <div class="row h-100">
-            <div class="col-md-6 hero-tagline my-auto">
+            <div class="col-md-7 hero-tagline mx-auto px-auto d-flex align-items-center flex-column">
                 <h1>KEAJAIBAN DI SETIAP HALAMAN</h1>
                 <p>Tempat terbaik untuk cerita dan petualangan anak-anak! Temukan dunia yang penuh warna, imajinasi, dan kegembiraan bersama <span class="fw-bold">Giggle</span>.</p>
-                <button class="button-lg-primary">^</button>
-                <a href=""><img src="asset/arrow-home.svg" alt=""></a>
+                <button class="btn button-lg-primary">
+                    <i class='bx bx-down-arrow-alt'></i>
+                </button>
             </div>
         </div>
+
+        <img src="{{ asset ('assets/images/img-hero-giggle.png') }}" alt="home" class="img-hero position-absolute start-0 bottom-0">
     </div>
 </section>
 
