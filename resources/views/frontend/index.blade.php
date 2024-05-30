14e7a58a5c7971a0dd2d34926a58d0e027e7059d
khadijahpassa <khadijahpassa@gmail.com>
2024-05-30 10:36:02 +0700

    odit commit

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Home Page')
+@section('content')
+
+<section id="hero">
+    <div class="container h-100">
+        <div class="row h-100">
+            <div class="col-md-6 hero-tagline my-auto">
+                <h1>KEAJAIBAN DI SETIAP HALAMAN</h1>
+                <p>Tempat terbaik untuk cerita dan petualangan anak-anak! Temukan dunia yang penuh warna, imajinasi, dan kegembiraan bersama <span class="fw-bold">Giggle</span>.</p>
+                <button class="button-lg-primary">^</button>
+                <a href=""><img src="asset/arrow-home.svg" alt=""></a>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Frontend\FrontendController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::controller(FrontendController::class)->group(function(){
+    Route::get('/', 'index');
 });
 
 Auth::routes();
