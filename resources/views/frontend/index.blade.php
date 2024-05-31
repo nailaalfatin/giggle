df2edd13cd5f7119a60c2967395b4f2f65bcde2c
ShikaDhiEr <ihavenoproblemshit@gmail.com>
2024-05-31 08:37:56 +0700

    shikade

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,6 +3,8 @@
 @section('title', 'Home Page')
 @section('content')
 
+
+
 <section id="hero">
     <div class="container h-100">
         <div class="row h-100">
@@ -10,12 +12,37 @@
                 <h1>KEAJAIBAN DI SETIAP HALAMAN</h1>
                 <p>Tempat terbaik untuk cerita dan petualangan anak-anak! Temukan dunia yang penuh warna, imajinasi, dan kegembiraan bersama <span class="fw-bold">Giggle</span>.</p>
                 <button class="btn button-lg-primary">
-                    <i class='bx bx-down-arrow-alt'></i>
+                    <i class='bx bx-chevron-down'></i>
                 </button>
             </div>
         </div>
 
         <img src="{{ asset ('assets/images/img-hero-giggle.png') }}" alt="home" class="img-hero position-absolute start-0 bottom-0">
+    </div>
+</section>
+
+<section>
+    <div class="carousel">
+        <div class="carousel-content">
+            <div class="carousel-item">
+                <img src="{{ asset ('assets/images/tes.png')}}" alt="Image">
+                <div class="carousel-text">
+                    <h2></h2>
+                    <p></p>
+                    <button class="start-reading bingah">Mulai Baca →</button>
+                </div>
+                <button class="carousel-button left"><i class='bx bx-chevron-left'></i></button>
+                <button class="carousel-button right"><i class='bx bx-chevron-right'></i></button>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <marquee class="footer-item bingah">giggle</marquee>
+        <marquee class="footer-item bingah">giggle</marquee>
+        <marquee class="footer-item bingah">giggle</marquee>
+        <marquee class="footer-item bingah">giggle</marquee>
+        <marquee class="footer-item bingah">giggle</marquee>
+        <marquee class="footer-item bingah">giggle</marquee>
     </div>
 </section>
 
